0dd5b7b7d5892e340bd2398eb2fdf3353762701d
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-02-26 15:48:18 +0900

    feat : PreQuestionCoach gpt api 에게 프롬프트를 보내 응답을 받아오는 기능 추가

--- habit/src/main/java/app/habit/config/AppConfig.java
+++ habit/src/main/java/app/habit/config/AppConfig.java
@@ -1,14 +1,22 @@
 package app.habit.config;
 
+import app.habit.domain.PreQuestionCoach;
+import app.habit.domain.HabitAssessmentManagerFactory;
 import app.habit.domain.PromptFactory;
 import app.habit.service.gpt.template.PromptTemplate;
 import app.habit.service.gpt.template.TemplateResolver;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     @Bean
     public TemplateResolver templateResolver() {
@@ -18,5 +26,10 @@ public class AppConfig {
     @Bean
     public PromptFactory promptFactory() {
         return new PromptFactory(templateResolver());
+    }
+
+    @Bean
+    public PreQuestionCoach coach() {
+        return new PreQuestionCoach(restTemplate());
     }
 }
